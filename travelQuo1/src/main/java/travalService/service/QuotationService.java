6308668c75fd2322a5786d474764a8308f28c667
JoyJiang98 <joyjiang1998@gmail.com>
2024-01-04 19:23:58 +0000

    secondfix

--- travelQuo1/src/main/java/travalService/service/QuotationService.java
+++
@@ -1,8 +0,0 @@
-package travalService.service;
-
-import travalService.model.QuotationRequest;
-import travalService.model.QuotationResponse;
-
-public interface QuotationService {
-    QuotationResponse generateQuotation(QuotationRequest request);
-}
